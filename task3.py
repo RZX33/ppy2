27721ad1093fc27810313c4c50b6b40aa9d88a11
RZX33 <s24794@pjwstk.edu.pl>
2023-03-18 20:07:38 +0100

    ver 1.0.

--- task3.py
+++ task3.py
@@ -1,16 +1,110 @@
-print('pytanie: Jak masz na imię oraz nazwisko?')
-input('odpowiedź: ')
-print('pytanie: Najczęstszym sposobem spędzania wolnego czasu jest dla Ciebie?')
-input('odpowiedź: ')
-print('pytanie: W jakich okolicznościach czytasz książki najczęściej??')
-input('odpowiedź: ')
-print('pytanie: Jeżeli spędzasz czas wolny czytając książki, jaki jest główny powód takiego wyboru?')
-input('odpowiedź: ')
-print('pytanie: Po książki w jakiej formie sięgasz najczęściej?')
-input('odpowiedź: ')
-print('pytanie: Ile książek czytasz średnio w ciągu roku?')
-input('odpowiedź: ')
-print('pytanie: Jak często średnio czytasz książki?')
-input('odpowiedź: ')
-print('pytanie: Po jakie gatunki książek sięgasz najczęściej?')
-input('odpowiedź: ')
+constant = 'odpowiedź: '
+question1 = 'pytanie: Jak masz na imię oraz nazwisko?'
+print(question1)
+answer1 = input('odpowiedź: ')
+question2 = 'pytanie: Najczęstszym sposobem spędzania wolnego czasu jest dla Ciebie?'
+answers2 =   '''
+             oglądanie telewizji/filmów/seriali 
+             czytanie książek/czasopism
+             słuchanie muzyki
+             spotkania z rodziną/przyjaciółmi
+             podróżowanie
+             uprawianie sportu
+            podczas podróży
+            '''
+print(question2)
+print(answers2)
+answer2 = input('odpowiedź: ')
+question3 = 'pytanie: W jakich okolicznościach czytasz książki najczęściej?'
+answers3 =  '''
+            w czasie wolnym (po pracy, na urlopie)
+            podczas pracy/nauki (to ich element)
+            w ogóle nie czytam
+            '''
+print(question3)
+print(answers3)
+answer3 = input('odpowiedź: ')
+question4 = 'pytanie: Jeżeli spędzasz czas wolny czytając książki, jaki jest główny powód takiego wyboru?'
+answers4 =  '''
+            chęć poszerzenia wiedzy
+            czytanie mnie relaksuje/odpręża
+            fakt, że czytanie jest modne
+            czytanie to moje hobby
+            konieczność nauki w związku z wykonywaną pracą/studiami
+            odczuwam presję rodziny/środowiska, żeby czytać           
+            '''
+print(question4)
+print(answers4)
+answer4 = input('odpowiedź: ')
+question5 = 'pytanie: Po książki w jakiej formie sięgasz najczęściej?'
+answers5 =  '''
+            papierowej (tradycyjnej)
+            e-booki (książki elektroniczne) na komputerze
+            e-booki na tablecie/telefonie
+            e-booki na specjalnym czytniku (np. Kindle)
+            '''
+print(question5)
+print(answers5)
+answer5 = input('odpowiedź: ')
+question6 = 'pytanie: Ile książek czytasz średnio w ciągu roku?'
+answers6 =  '''
+            0
+            żadnej w całości - jedynie fragmenty	
+            1
+            2 lub 3	
+            4-10
+            powyżej 10 
+            '''
+print(question6)
+print(answers6)
+answer6 = input('odpowiedź: ')
+question7 = 'pytanie: Jak często średnio czytasz książki?'
+answers7 =  '''
+            Można udzielić jednej odpowiedzi.
+            codziennie            	
+            raz w tygodniu            	
+            raz w miesiącu            	
+            raz na kilka miesięcy	
+            raz na pół roku            	
+            raz na rok            	
+            wcale 
+            '''
+print(question7)
+print(answers7)
+answer7 = input('odpowiedź: ')
+question8 = 'pytanie: Po jakie gatunki książek sięgasz najczęściej?'
+answers8 =  '''
+            kryminały/thrillery	
+            romanse            	
+            psychologiczne
+            horrory            	
+            naukowe            	
+            dla dzieci i młodzieży
+            fantastykę            	
+            biograficzne            	
+            historyczne
+            science fiction            	
+            podróżnicze            	
+            hobbystyczne (gotowanie, wędkarstwo itp.)
+            przygodowe            	
+            poezję 
+            '''
+print(question8)
+print(answers8)
+answer8 = input('odpowiedź: ')
+print(question1)
+print(constant, answer1)
+print(question2)
+print(constant, answer2)
+print(question3)
+print(constant, answer3)
+print(question4)
+print(constant, answer4)
+print(question5)
+print(constant, answer5)
+print(question6)
+print(constant, answer6)
+print(question7)
+print(constant, answer7)
+print(question8)
+print(constant, answer8)
